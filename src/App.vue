133353224dfc1f72072c15b5a79725d2991a79e0
vvvvv <563915041@qq.com>
2021-08-30 00:45:42 +0800

    button

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <vv-button>按钮</vv-button>
+    <vv-button type="primary">登陆</vv-button>
+    <vv-button type="success">登陆</vv-button>
+    <vv-button type="info">登陆</vv-button>
+    <vv-button type="warning">登陆</vv-button>
+    <vv-button type="danger">登陆</vv-button>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VvButton from './components/button.vue'
+
+Vue.component(VvButton.name, VvButton)
+
 Vue.config.productionTip = false
 
 new Vue({
